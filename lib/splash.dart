13b9ac3b6817bc10ddb96ea161a238eb6c917b94
sarojyadav <sarojyadav88505@gmail.com>
2020-09-05 11:57:59 +0545

    first commit

---
+++ lib/splash.dart
@@ -0,0 +1,28 @@
+import 'package:calculate_magii/main.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Calculator()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(
+        "assets/images/splash.png",
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
